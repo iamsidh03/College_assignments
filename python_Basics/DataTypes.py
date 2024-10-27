23150e000dcd1c12cfb5a610ae9d6ba4c65f5e4d
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2024-10-27 12:55:23 +0000

    jgdj

---
+++ python_Basics/DataTypes.py
@@ -0,0 +1,32 @@
+'''
+Python has the following data types built-in by default, in these categories:
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+'''
+
+#type()---> gives you the data type.
+
+# x = 1j
+# print(x)
+# print(type(x))
+
+'''
+  Integer datatype: Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+  Float datatype:Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+  '''
+# x=9999999999999999999999999999; #can be any length
+# print(x,type(x))
+
+# y=-3434.43434
+# print(y)
+
+#Float can also be scientific numbers with an "e" to indicate the power of 10.
+# f=123e2 # equivalent to 123100
+
